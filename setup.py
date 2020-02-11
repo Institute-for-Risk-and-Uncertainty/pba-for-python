7eb4cc651b87f8b3e9417f30c6d478d0b939dba9
Nick <nickgray@liv.ac.uk>
2020-02-11 12:29:29 +0000

    v0.4.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pba',
-    version='0.4.dev4',
+    version='0.4.4',
     packages=['pba',],
     license='MIT License',
     long_description=open('README.rst').read(),
