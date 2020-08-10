7f77288c8afa73d4a0bcd5768f2681bfaa851ffc
Nick <nickgray@liverpool.ac.uk>
2020-08-10 10:45:01 +0100

    v0.7.2 Added copulas Added to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pba',
-    version='0.7.1',
+    version='0.7.2',
     packages=['pba',],
     license='MIT License',
     long_description=open('README.rst').read(),
