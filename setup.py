1414a6ac7e480a385e220fde0bf86804210861ea
Nick <nickgray@liverpool.ac.uk>
2022-03-24 15:37:30 +0000

    update req.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pba',
-    version='test',
+    version='v0.11.1',
     packages=['pba',],
     license='MIT License',
     long_description=open('README.rst').read(),
