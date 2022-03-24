f66daf479f39cd1b289a099345bc620bcdace741
Dominic Calleja <sgdcalle@student.liverpool.ac.uk>
2022-03-24 19:46:32 +0000

    mod progress

--- pba/_dists.py
+++ pba/_dists.py
@@ -1,3 +1,7 @@
+if __name__ is not None and "." in __name__:
+    from .interval import Interval
+else:
+    from interval import Interval
 import json
 
 import scipy.stats as sps
@@ -10,14 +14,7 @@ extra = {
     'uniform': sps.uniform,
 }
 
-dists = []
-for k, v in simple_dists.items():
-    dists.append(k)
-
-d = {'dists':dists}
-with open('/Users/dominiccalleja/pba-for-python/pba/data.json', 'w') as j:
-    json.dump(d, j)
-
+dist = read_json('data.json')['dists']
 
 def __get_bounds(function_name=None, steps=200, *args):
 
@@ -61,13 +58,139 @@ def __get_bounds(function_name=None, steps=200, *args):
 
 
 class Parametric():
-
-    def __init__():
-
-    def _pdf():
-
-    def _cdf():
-
-    def _ppf():
+    """
+    A parametric Pbox is defined where parameters of a named distribtuion are specified as
+    Intervals. 
     
-    def 
+    Parametric can be created using::
+        pba.I(left,right)
+        pba.Interval(left,right)
+        
+    Parameters
+    ----------
+    left : numeric
+        left side of interval
+    right : numeric
+        right side of interval
+        
+    Attributes
+    ----------
+    left : numeric
+        left side of interval
+    right : numeric
+        right side of interval
+
+    """
+    params = []
+
+    def __init__(self, distribution, pbox=True, *args, **kwargs):
+        self._parameter_list = list_parameters(distribution)
+        self.distribution = distribution
+
+        if args:
+            self.set_from_args(*args)
+        if kwargs:
+            self.set_parameters(**kwargs)
+
+        self._pbox = self._pba_constructor(*args)
+        return None
+
+    def _pdf(self):
+        return None
+
+    def _cdf(self):
+        return None
+
+    def _ppf(self):
+        return None
+
+    def _logpdf(self):
+        return None
+
+
+    def _pba_constructor(self, *args):
+        args = list(args)
+
+        for i in range(len(args)):
+            if args[i].__class__.__name__ != 'Interval':
+                args[i] = Interval(args[i])
+
+        Left, Right, mean, var = __get_bounds(self.distribution, steps, *args)
+        return Pbox(
+            Left,
+            Right,
+            steps      = steps,
+            shape      = self.distribution,
+            mean_left  = mean.left,
+            mean_right = mean.right,
+            var_left   = var.left,
+            var_right  = var.right
+            )
+    def set_from_args(self,*args):
+        self._parameter_list
+        args = list(args)
+        for i, v in enumerate(args):
+            d = {self._parameter_list[i]:v}
+            self._set_parameter(**d)
+
+    def set_parameters(self, **kwargs):
+        if kwargs:
+            self._set_parameter(**kwargs)
+
+    def _set_parameter(self, **kwargs):
+        if kwargs:
+            for k, v in kwargs.items():
+                assert k in self._parameter_list, '{} not in param list: {}'.format(
+                    k, param)
+                if not isinstance(v, Interval):
+                    v = Interval(v)
+                setattr(self, k, v)
+    
+    def __getattr__(self, name):
+        try:
+            return getattr(self._pbox, name)
+        except AttributeError:
+            raise AttributeError("Constructor' object has no attribute '%s'" % name)
+
+def check_implimentation(distribution):
+    if distribution in dist:
+        return True
+    else:
+        sys.exit('{} not implimented, choose form : \n\n{}'.format(
+            distribution, availiable_distributions()))
+        return False
+
+def availiable_distributions():
+    return [' {},'.format(d) for d in dist]
+
+def list_parameters(distribution):
+    """List parameters for stats.distribution.
+    
+    Parameters
+    ----------
+        distribution: a string or stats distribution object.
+    Output
+    ------
+        A list of distribution parameter strings.
+    """
+    if isinstance(distribution, str):
+        try:
+            distribution = getattr(sps, distribution)
+        except:
+            check_implimentation(distribution)
+    if distribution.shapes:
+        parameters = [name.strip() for name in distribution.shapes.split(',')]
+    else:
+        parameters = []
+    if distribution.name in sps._discrete_distns._distn_names:
+        parameters.insert(0,'loc')
+    elif distribution.name in sps._continuous_distns._distn_names:
+        parameters.insert(0,'loc')
+        parameters.insert(1,'scale')
+    return parameters
+
+
+def read_json(file_name):
+    f = open(file_name)
+    data = json.load(f)
+    return data
