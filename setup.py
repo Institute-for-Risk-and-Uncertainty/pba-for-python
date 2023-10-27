cb35868577b1e73630f9fff88b048905b99bfa91
Nick <nickgray@liverpool.ac.uk>
2023-10-27 13:42:58 +0100

    v0.16 various fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pba',
-    version='1.beta2',
+    version='1.beta3',
     packages=['pba',],
     license='MIT License',
     long_description=open('README.rst').read(),
